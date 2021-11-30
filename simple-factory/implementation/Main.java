78ef253b481c39bac6a4585fc091230e55e527cc
Tommi Ahonen <tommi.t.ahonen@gmail.com>
2021-11-30 18:47:24 +0100

    Add printout to main method.

--- simple-factory/implementation/Main.java
+++ simple-factory/implementation/Main.java
@@ -4,5 +4,8 @@ public class Main {
         Character c1 = cFactory.createCharacter("Lillfjant", CharacterType.TROLL, WeaponType.CLUB);
         Character c2 = cFactory.createCharacter("Sir James", CharacterType.KNIGHT, WeaponType.SWORD);
         Character c3 = cFactory.createCharacter("Fistfighting Black Knight", CharacterType.KNIGHT, WeaponType.UNARMED);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
     }
 }
